15c5499906a840d1591b8437a8f1c01e2a4921e8
Donald <1376825153@qq.com>
2020-03-21 19:32:13 +0800

    引入图片并使用

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,20 @@
 import React, { Component,Fragment } from 'react';
 import { Carousel } from 'antd-mobile';
 import axios,{baseURL} from '../../utils/request';
+import nav1 from '../../assets/images/nav-1.png';
+import nav2 from '../../assets/images/nav-2.png';
+import nav3 from '../../assets/images/nav-3.png';
+import nav4 from '../../assets/images/nav-4.png';
 class index extends Component {
   state={
     carousel:[],
     imgHeight:176,
+    navs:[
+      {id:0,text:'整租',img:nav1},
+      {id:1,text:'合租',img:nav2},
+      {id:2,text:'地图找房',img:nav3},
+      {id:3,text:'去出租',img:nav4}
+    ]
   };
   async componentDidMount(){
    const res=await axios.get('/home/swiper')
@@ -39,6 +49,12 @@ class index extends Component {
               </a>
             ))}
           </Carousel>
+        </div>
+        <div className='index_navs'>
+          {this.state.navs.map(v=><div className='navs_item' key={v.id}>
+            <img src={v.img} alt=''></img>
+            <p>{v.text}</p>
+          </div>)}
         </div>
       </div>
     );
