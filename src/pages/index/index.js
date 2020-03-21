9dcb508da1c22293e6a883cd7b52e14ced040437
Donald <1376825153@qq.com>
2020-03-21 18:26:21 +0800

    请求数据 实现轮播效果

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,9 +1,46 @@
 import React, { Component,Fragment } from 'react';
+import { Carousel } from 'antd-mobile';
+import axios from 'axios';
 class index extends Component {
+  state={
+    carousel:[],
+    imgHeight:176,
+  };
+  async componentDidMount(){
+   const res=await axios.get('http://157.122.54.189:9060/home/swiper')
+   console.log(res)
+   this.setState({
+     carousel:res.data.body
+   })
+  }
   render() {
     return (
-      <div>
-        122
+      <div >
+        <div className='index_carousel'>
+          <Carousel
+            autoplay
+            infinite
+          >
+            {this.state.carousel.map(val => (
+              <a
+                key={val}
+                href=""
+                style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+              >
+                <img
+                  src={`http://157.122.54.189:9060`+val.imgSrc}
+                  alt=""
+                  style={{ width: '100%', verticalAlign: 'top' }}
+                  onLoad={() => {
+                    // fire window resize event to change height
+                    window.dispatchEvent(new Event('resize'));
+                    this.setState({ imgHeight: 'auto' });
+                  }}
+                />
+              </a>
+            ))}
+          </Carousel>
+        </div>
       </div>
     );
   }
