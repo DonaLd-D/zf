d8c3cafbc89f4e20b92be255d6f04c6b1cf93e19
Donald <1376825153@qq.com>
2020-03-22 18:50:20 +0800

    首页渲染完成

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -15,13 +15,28 @@ class index extends Component {
       {id:1,text:'合租',img:nav2},
       {id:2,text:'地图找房',img:nav3},
       {id:3,text:'去出租',img:nav4}
-    ]
+    ],
+    group:[],
+    news:[]
   };
   async componentDidMount(){
-   const res=await axios.get('/home/swiper')
-   this.setState({
-     carousel:res.data.body
-   })
+   this.getCarousel();
+   this.getGroup();
+   this.getNews();
+  }
+
+  getCarousel=async ()=>{
+    const res=await axios.get('/home/swiper')
+    this.setState({carousel:res.data.body})
+  }
+  getGroup=async ()=>{
+    const res=await axios.get('/home/groups?area=AREA%7C88cff55c-aaa4-e2e0')
+    this.setState({group:res.data.body})
+  }
+  getNews=async ()=>{
+    const res=await axios.get('/home/news?area=AREA%7C88cff55c-aaa4-e2e0')
+    console.log(res)
+    this.setState({news:res.data.body})
   }
   render() {
     return (
@@ -51,12 +66,43 @@ class index extends Component {
             ))}
           </Carousel>
         </div>
-        <div className='index_navs'>
-          {this.state.navs.map(v=><div className='navs_item' key={v.id}>
+
+        <div className={indexCss.index_navs}>
+          {this.state.navs.map(v=><div className={indexCss.navs_item} key={v.id}>
             <img src={v.img} alt=''></img>
             <p>{v.text}</p>
           </div>)}
         </div>
+
+        <div className={indexCss.index_group}>
+          <div className={indexCss.index_group_title}><span>租房小组</span><a href="">更多</a></div>
+          <div className={indexCss.index_group_content}>
+            {this.state.group.map(v=><div className={indexCss.group_item} key={v.id}>
+              <div className={indexCss.goup_content_left}>
+               <div>{v.title}</div>
+               <div>{v.desc}</div>
+              </div>
+              <div className={indexCss.goup_content_right}>
+                <img src={baseURL+v.imgSrc} alt=""/>
+              </div>
+            </div>)}
+          </div>
+        </div>
+
+        <div className={indexCss.index_news}>
+          <h3>最新资讯</h3>
+          <div className={indexCss.index_news_content}>
+            {this.state.news.map(v=><div className={indexCss.news_item} key={v.id}>
+              <div className={indexCss.item_image}>
+                <img src={baseURL+ v.imgSrc} alt=""/>
+              </div>
+              <div className={indexCss.item_right}>
+                <p>{v.title}</p>
+                <div><a href="">{v.from}</a><span>{v.date}</span></div>
+              </div>
+            </div>)}
+          </div>
+        </div> 
       </div>
     );
   }
