27b496958061d40b2aa3bfa65d7874a7f68cceff
Donald <1376825153@qq.com>
2020-03-24 15:48:48 +0800

    cityfound和mapfound添加navbar

--- src/pages/cityfound/cityfound.js
+++ src/pages/cityfound/cityfound.js
@@ -1,9 +1,14 @@
 import React, { Component ,Fragment} from 'react';
+import {NavBar,Icon} from 'antd-mobile';
 class city extends Component {
   render() {
     return (
       <div>
-        <Fragment>cityfound</Fragment>
+        <NavBar
+        mode="light"
+        icon={<Icon type="left" />}
+        onLeftClick={() => this.props.history.go(-1)}
+        >城市选择</NavBar>
       </div>
     );
   }
--- src/pages/mapfound/mapfound.js
+++ src/pages/mapfound/mapfound.js
@@ -1,9 +1,14 @@
 import React, { Component ,Fragment} from 'react';
+import {NavBar,Icon} from 'antd-mobile';
 class map extends Component {
   render() {
     return (
       <div>
-        <Fragment>mapfound</Fragment>
+        <NavBar
+        mode="light"
+        icon={<Icon type="left" />}
+        onLeftClick={() => this.props.history.go(-1)}
+        >城市选择</NavBar>
       </div>
     );
   }
