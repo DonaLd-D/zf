09a50e788cf1c36623421d4aa7455b5f386d0ac3
Donald <1376825153@qq.com>
2020-03-24 11:54:01 +0800

    改造类组件为函数式组件

--- src/components/cityinput/cityinput.js
+++ src/components/cityinput/cityinput.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import indexCss from './cityinput.module.scss'
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-class cityinput extends Component {
-  render() {
-    const {history,cityName}=this.props;
-    return (
-      <div className={indexCss.city_input}>
-        <div className={indexCss.input_content}>
-          <div className={indexCss.input_wrap} onClick={()=>history.push('/cityfound')}>
-            <span>{cityName}</span>
-            <i className={['iconfont','icon-arrow'].join(' ')}></i>
-          </div>
-          <div className={indexCss.input_address}>
-            <i className={['iconfont', 'icon-seach'].join(' ')}></i>
-            <span>请输入小区或地址</span>
-          </div>
+const cityinput=React.memo((props)=>{
+  
+  const {history,cityName}=props;
+  return (
+    <div className={indexCss.city_input}>
+      <div className={indexCss.input_content}>
+        <div className={indexCss.input_wrap} onClick={()=>history.push('/cityfound')}>
+          <span>{cityName}</span>
+          <i className={['iconfont','icon-arrow'].join(' ')}></i>
         </div>
-        <div className={indexCss.input_map} onClick={()=>history.push('/mapfound')}>
-          <i className={['iconfont', 'icon-map'].join(' ')}></i>
+        <div className={indexCss.input_address}>
+          <i className={['iconfont', 'icon-seach'].join(' ')}></i>
+          <span>请输入小区或地址</span>
         </div>
       </div>
-    );
-  }
-}
+      <div className={indexCss.input_map} onClick={()=>history.push('/mapfound')}>
+        <i className={['iconfont', 'icon-map'].join(' ')}></i>
+      </div>
+    </div>
+  );
+})
+
 
 const mapStateToProps=(state)=>{
   return {
